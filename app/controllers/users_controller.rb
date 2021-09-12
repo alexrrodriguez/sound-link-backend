b1f5b8bba8a0f5c0bb284f5a7ce0f3d8c0b96d21
alex <arodriguez202020@gmail.com>
2021-09-12 13:04:22 -0500

    create index and update method for current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+  def index
+    user = current_user
+    render json: user
+  end
+
   def create
     user = User.new(
       name: params[:name],
@@ -12,4 +18,17 @@ class UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
+
+  def update
+    user = User.find_by(id: params[:id])
+    user.name = params[:name] || user.name
+    user.email = params[:email] || user.email
+    user.image = params[:image] || user.image
+    if user.save
+      render json: user
+    else 
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+   
 end
