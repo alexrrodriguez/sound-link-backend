0380c1a97ce6b382403f80f1f87574a7e64790a6
alex <arodriguez202020@gmail.com>
2021-09-12 13:06:38 -0500

    add users index and patch routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
+  get "/users" => "users#index"
+
   post "/users" => "users#create"
+
+  patch "/users/:id" => "users#update"
 
   post "/sessions" => "sessions#create"
 
