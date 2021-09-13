1a65bc26d2a508da45e568625375860cc33c9a5d
alex <arodriguez202020@gmail.com>
2021-09-13 10:36:44 -0500

    adds relationship to user, concert, and user_concert models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
-  has_many :concerts
+  has_many :user_concerts
+  has_many :concerts, through: :user_concerts
 end
