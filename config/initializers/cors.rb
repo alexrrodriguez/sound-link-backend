becca27bda02057c40f3161d52abf184808df77b
alex <arodriguez202020@gmail.com>
2021-09-24 14:34:56 -0500

    add ticketmaster base url to cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', '*', 'https://peaceful-noyce-4769d0.netlify.app'
+    origins 'localhost:8080', '*', 'https://peaceful-noyce-4769d0.netlify.app', 'https://app.ticketmaster.com'
 
     resource '*',
       headers: :any,
